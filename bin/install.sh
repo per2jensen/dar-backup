d6fc595dfa2d6a0a34ffc2171c69d9ed0140c8ab
Per Jensen <per2jensen@gmail.com>
2021-07-04 11:31:43 +0200

    use the template dir

--- bin/install.sh
+++ bin/install.sh
@@ -12,9 +12,6 @@ chmod +x ${SCRIPTDIRPATH}/dar-backup.sh ${SCRIPTDIRPATH}/../conf/*.duc
 
 (cd "$SCRIPTDIRPATH"; rm dar-diff-backup.sh; ln -s dar-backup.sh  dar-diff-backup.sh) 
 
-sed -e "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|" $SCRIPTDIRPATH/../conf/dar_par.dcf.template > $SCRIPTDIRPATH/../conf/dar_par.dcf
-sed -e "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|" $SCRIPTDIRPATH/../conf/darrc.template       > $SCRIPTDIRPATH/../conf/darrc
-
-sed -i "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|"  $SCRIPTDIRPATH/../backups.d/darrc-pj-homedir
-
-
+sed -e "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|" $SCRIPTDIRPATH/../templates/dar_par.dcf.template   > $SCRIPTDIRPATH/../conf/dar_par.dcf
+sed -e "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|" $SCRIPTDIRPATH/../templates/darrc.template         > $SCRIPTDIRPATH/../conf/defaults-rc
+sed -e "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|" $SCRIPTDIRPATH/../templates/TEST                   > $SCRIPTDIRPATH/../backups.d/TEST
