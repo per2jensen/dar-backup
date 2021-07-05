c8e8dcde4aaa7dce4f937c880a712f5bedd386c6
Per Jensen <per2jensen@gmail.com>
2021-07-05 22:39:10 +0200

    take 1 on automated test

---
+++ test/run-test.sh
@@ -0,0 +1,47 @@
+#! /bin/bash -x
+
+# run install.sh
+# run dar-backup.sh
+# add file GREENLAND.JEP to include dir and to the exclude dir
+# run dar-diff-backup.sh
+# list the FULL & DIFF archives
+
+SCRIPTPATH=`realpath $0`
+SCRIPTDIRPATH=`dirname $SCRIPTPATH`
+DATE=`date +"%Y-%m-%d"`
+
+TESTDIR=/tmp/dar-backup-test
+
+rm -fr $TESTDIR
+rm -fr ~/mnt/TEST/*
+mkdir -p $TESTDIR
+
+cp -R $SCRIPTDIRPATH/dirs         $TESTDIR/
+cp -R $SCRIPTDIRPATH/../bin       $TESTDIR/
+cp -R $SCRIPTDIRPATH/../conf      $TESTDIR/
+
+source $TESTDIR/conf/dar-backup.conf
+
+# create templates dir and copy it
+cp -R $SCRIPTDIRPATH/templates    $TESTDIR/
+cp $SCRIPTDIRPATH/../templates/dar_par.dcf.template $TESTDIR/templates/
+cp $SCRIPTDIRPATH/../templates/darrc.template       $TESTDIR/templates/
+
+
+# install and run FULL backup
+(cd $TESTDIR/bin; chmod +x install.sh  &&  $TESTDIR/bin/install.sh  &&  $TESTDIR/bin/dar-backup.sh)
+dar -l  "$MOUNT_POINT/TEST_FULL_$DATE" > $TESTDIR/FULL-filelist.txt
+
+
+# alter backup set
+cp GREENLAND.JPEG "$TESTDIR/dirs/include this one/"
+cp GREENLAND.JPEG "$TESTDIR/dirs/exclude this one/"
+
+# run DIFF backup
+(cd $TESTDIR/bin  &&  $TESTDIR/bin/dar-diff-backup.sh)
+dar -l  "$MOUNT_POINT/TEST_DIFF_$DATE" > $TESTDIR/DIFF-filelist.txt
+
+
+cat /tmp/dar-backup-test/FULL-filelist.txt 
+cat /tmp/dar-backup-test/DIFF-filelist.txt 
+
