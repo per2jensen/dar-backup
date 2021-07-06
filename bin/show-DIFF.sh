4eafe986f6050d9556a1f5476360e29c870546bc
Per Jensen <per2jensen@gmail.com>
2021-07-06 15:33:53 +0200

    nicer output

--- bin/show-DIFF.sh
+++ bin/show-DIFF.sh
@@ -12,8 +12,9 @@ source "${SCRIPTDIRPATH}/dar-util.sh"
 mountPrereqs
 
 
+echo Mountpoint: ${MOUNT_POINT}
 for archive in `ls ${MOUNT_POINT}/*_DIFF_*.dar|egrep "^.*?[0-9]{4}-[0-9]{2}-[0-9]{2}" -o|sort -u`; do
     BASE=`basename ${archive}`
     NO_SLICES=`find ${MOUNT_POINT} -name "${BASE}*.dar"|wc -l`
-    echo ${archive}: Number of slices: ${NO_SLICES} 
+    printf "%-30s slices: %-5s\n" ${BASE} ${NO_SLICES}
 done
