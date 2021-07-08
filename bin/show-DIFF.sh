aa613a2e0721448dec31d641f68641df52b0f3bb
Per Jensen <per2jensen@gmail.com>
2021-07-08 14:06:53 +0200

    indicate slice sizes

--- bin/show-DIFF.sh
+++ bin/show-DIFF.sh
@@ -1,5 +1,5 @@
 #! /bin/bash 
-
+# print FULL archives including  number of slices and the slice size
 
 export DATE=`date -I`
 export SCRIPTPATH=`realpath "$0"`
@@ -11,10 +11,10 @@ source "${SCRIPTDIRPATH}/dar-util.sh"
 # make sure mounts are in order
 mountPrereqs
 
-
 echo Mountpoint: ${MOUNT_POINT}
 for archive in `ls ${MOUNT_POINT}/*_DIFF_*.dar|egrep "^.*?[0-9]{4}-[0-9]{2}-[0-9]{2}" -o|sort -u`; do
     BASE=`basename ${archive}`
     NO_SLICES=`find ${MOUNT_POINT} -name "${BASE}*.dar"|wc -l`
-    printf "%-30s slices: %-5s\n" ${BASE} ${NO_SLICES}
+    SLICE_SIZE=`ls -l --block-size=G "${MOUNT_POINT}/${BASE}.1.dar"|cut -d" " -f5`
+    printf "%-30s slices: %-3s (%s)\n" ${BASE} ${NO_SLICES} ${SLICE_SIZE}
 done
--- bin/show-FULL.sh
+++ bin/show-FULL.sh
@@ -1,4 +1,5 @@
 #! /bin/bash 
+# print FULL archives including  number of slices and the slice size
 
 export DATE=`date -I`
 export SCRIPTPATH=`realpath "$0"`
@@ -15,5 +16,6 @@ echo Mountpoint: ${MOUNT_POINT}
 for archive in `ls ${MOUNT_POINT}/*_FULL_*.dar|egrep "^.*?[0-9]{4}-[0-9]{2}-[0-9]{2}" -o|sort -u`; do
     BASE=`basename ${archive}`
     NO_SLICES=`find ${MOUNT_POINT} -name "${BASE}*.dar"|wc -l`
-    printf "%-30s slices: %-5s\n" ${BASE} ${NO_SLICES}
+    SLICE_SIZE=`ls -l --block-size=G "${MOUNT_POINT}/${BASE}.1.dar"|cut -d" " -f5`
+    printf "%-30s slices: %-3s (%s)\n" ${BASE} ${NO_SLICES} ${SLICE_SIZE}
 done
