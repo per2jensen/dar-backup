e8b9b7c2c2b0969c958b42a5c5664f4d27d6645a
Per Jensen <per2jensen@gmail.com>
2022-04-10 16:49:02 +0200

    debug action

--- test/test-install-package.sh
+++ test/test-install-package.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/bash -x
 
 #
 # Build tar file, run install process, execute install backup definition
@@ -9,6 +9,7 @@ export RESULT=0
 
 SCRIPTPATH=$(realpath "$0")
 SCRIPTDIRPATH=$(dirname "$SCRIPTPATH")
+
 
 git push origin --delete "$TAG" > /dev/null 2>&1
 git tag -a -f  -m "unit test of installer" "$TAG"
@@ -22,12 +23,20 @@ rm -fr /tmp/dar-backup
 tar zxf /tmp/dar-backup-linux-"${TAG}".tar.gz --directory /tmp
 chmod +x /tmp/dar-backup/bin/install.sh
 /tmp/dar-backup/bin/install.sh
+
 /tmp/dar-backup/bin/dar-backup.sh --local-backup-dir
 if [[ $? != "0" ]]; then
-    echo "ERROR delivered backup definition failed, exiting"
+    echo "ERROR delivered backup definition failed"
     RESULT=1
 fi
 cat /tmp/dar-backup/archives/dar-backup.log
+
+echo "The archive and log file should be here somewhere....."
+find /tmp/dar-backup "*" -ls
+
+echo " "
+echo "The restored file should be here somewhere....."
+find /tmp/dar-restore/ "*" -ls
 
 echo "RESULT: $RESULT"
 exit "$RESULT"
