b34312ea6f01aae5699e1ef29e04ec5c8f6b9792
Per Jensen <per2jensen@gmail.com>
2022-04-10 09:22:14 +0200

    refac installer (shecllcheck linter)

--- bin/install.sh
+++ bin/install.sh
@@ -10,12 +10,13 @@ _backup_file () {
   fi
 }
 
-SCRIPTPATH=`realpath $0`
-SCRIPTDIRPATH=`dirname $SCRIPTPATH`
+SCRIPTPATH=$(realpath "$0")
+SCRIPTDIRPATH=$(dirname "$SCRIPTPATH")
 
-chmod +x ${SCRIPTDIRPATH}/*.sh ${SCRIPTDIRPATH}/../conf/*.duc 
+chmod +x "${SCRIPTDIRPATH}/*.sh" "${SCRIPTDIRPATH}/../conf/*.duc" 
 
-mkdir "$SCRIPTDIRPATH"/../archives > /dev/null 2>&1
+ARCHIVE_DIR="$SCRIPTDIRPATH/../archives"
+mkdir "$ARCHIVE_DIR" > /dev/null 2>&1
 
 
 (cd "$SCRIPTDIRPATH"; rm dar-diff-backup.sh > /dev/null 2>&1; ln -s dar-backup.sh dar-diff-backup.sh) 
@@ -23,19 +24,19 @@ mkdir "$SCRIPTDIRPATH"/../archives > /dev/null 2>&1
 
 FILE="$SCRIPTDIRPATH/../conf/dar_par.dcf"
 _backup_file "$FILE"
-sed -e "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|" $SCRIPTDIRPATH/../templates/dar_par.dcf.template   > "$FILE"
+sed -e "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|" "$SCRIPTDIRPATH/../templates/dar_par.dcf.template"   > "$FILE"
 
 FILE="$SCRIPTDIRPATH/../conf/defaults-rc"
 _backup_file "$SCRIPTDIRPATH/../conf/defaults-rc"
-sed -e "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|" $SCRIPTDIRPATH/../templates/darrc.template         > "$FILE"
+sed -e "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|" "$SCRIPTDIRPATH/../templates/darrc.template"         > "$FILE"
 
-sed --in-place=.org "s|@@ARCHIVE_DIR@@|${SCRIPTDIRPATH}/..|" $SCRIPTDIRPATH/../conf/dar-backup.conf 
+sed --in-place=.org "s|@@ARCHIVE_DIR@@|${SCRIPTDIRPATH}/../|" "$SCRIPTDIRPATH/../conf/dar-backup.conf" 
 
 
 if [ ! -d "$SCRIPTDIRPATH/../backups.d" ]; then
   mkdir "$SCRIPTDIRPATH/../backups.d"
 fi
-for file in $SCRIPTDIRPATH/../templates/backups.d/*; do
-    base=`basename $file`
+for file in "$SCRIPTDIRPATH"/../templates/backups.d/*; do
+    base=$(basename "$file")
     sed -e "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|" "$SCRIPTDIRPATH/../templates/backups.d/$base"  > "$SCRIPTDIRPATH/../backups.d/$base"
 done
