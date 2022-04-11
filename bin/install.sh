1925c8910c8172c6b9134d2ce54522925b76ddee
Per Jensen <per2jensen@gmail.com>
2022-04-11 09:28:54 +0200

    no inline trickery on dar-backup.conf

--- bin/install.sh
+++ bin/install.sh
@@ -22,15 +22,18 @@ mkdir "$ARCHIVE_DIR" > /dev/null 2>&1
 (cd "$SCRIPTDIRPATH"; rm dar-diff-backup.sh > /dev/null 2>&1; ln -s dar-backup.sh dar-diff-backup.sh) 
 (cd "$SCRIPTDIRPATH"; rm dar-inc-backup.sh  > /dev/null 2>&1; ln -s dar-backup.sh dar-inc-backup.sh)
 
+#template files
 FILE="$SCRIPTDIRPATH/../conf/dar_par.dcf"
 _backup_file "$FILE"
-sed -e "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|" "$SCRIPTDIRPATH/../templates/dar_par.dcf.template"   > "$FILE"
+sed -e "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|" "$SCRIPTDIRPATH/../templates/dar_par.dcf.template" > "$FILE"
 
 FILE="$SCRIPTDIRPATH/../conf/defaults-rc"
-_backup_file "$SCRIPTDIRPATH/../conf/defaults-rc"
-sed -e "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|" "$SCRIPTDIRPATH/../templates/darrc.template"         > "$FILE"
+_backup_file "$FILE"
+sed -e "s|@@CONFDIR@@|${SCRIPTDIRPATH}/../conf|" "$SCRIPTDIRPATH/../templates/darrc.template" > "$FILE"
 
-sed --in-place=.org "s|@@ARCHIVE_DIR@@|${SCRIPTDIRPATH}/../|" "$SCRIPTDIRPATH/../conf/dar-backup.conf" 
+FILE="$SCRIPTDIRPATH/../conf/dar-backup.conf"
+_backup_file "$FILE"
+sed -e "s|@@ARCHIVE_DIR@@|${SCRIPTDIRPATH}/../|" "$SCRIPTDIRPATH/../templates/dar-backup.conf" > "$FILE"        
 
 
 if [ ! -d "$SCRIPTDIRPATH/../backups.d" ]; then
