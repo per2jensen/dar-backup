6c581e3557119abffb949e434c578fa2ff4e6802
Per Jensen <per2jensen@gmail.com>
2022-04-13 21:25:44 +0200

    list number of added & removed files

--- bin/ls-archives.sh
+++ bin/ls-archives.sh
@@ -1,8 +1,8 @@
-#! /bin/bash 
+#! /bin/bash
 
-export DATE=`date -I`
-export SCRIPTPATH=`realpath "$0"`
-export SCRIPTDIRPATH=`dirname "$SCRIPTPATH"`
+SCRIPTPATH=$(realpath "$0")
+SCRIPTDIRPATH=$(dirname "$SCRIPTPATH")
+echo SCRIPTDIRPATH: "$SCRIPTDIRPATH"
 
 source "${SCRIPTDIRPATH}/../conf/dar-backup.conf"
 source "${SCRIPTDIRPATH}/dar-util.sh"
@@ -10,13 +10,24 @@ source "${SCRIPTDIRPATH}/dar-util.sh"
 # make sure mounts are in order
 mountPrereqs
 
+FILELIST="/tmp/dar-354Ay2534-filelist.txt"
 echo Mountpoint: ${MOUNT_POINT}
-for archive in `ls ${MOUNT_POINT}/*.dar|egrep "*_FULL_*|*_DIFF_*|*_INC_*"|egrep "^.*?[0-9]{4}-[0-9]{2}-[0-9]{2}" -o|sort -u`; do
-    BASE=`basename ${archive}`
-    NO_SLICES=`find ${MOUNT_POINT} -name "${BASE}*.dar"|wc -l`
-    SLICE_SIZE=`ls -l --block-size=G "${MOUNT_POINT}/${BASE}.1.dar"|cut -d" " -f5`
-    DISK_USAGE_TOTAL=`du -sch ${MOUNT_POINT}/${BASE}.*|egrep "total"|cut -f1`
-    printf "%-30s slices: %-3s (%s)   total: %-10s  \n" ${BASE} ${NO_SLICES} ${SLICE_SIZE} ${DISK_USAGE_TOTAL}
+for archive in $(ls ${MOUNT_POINT}/*.dar|grep -E "*_FULL_*|*_DIFF_*|*_INC_*"|grep -E "^.*?[0-9]{4}-[0-9]{2}-[0-9]{2}" -o|sort -u); do
+    BASE=$(basename ${archive})
+    NO_SLICES=$(find ${MOUNT_POINT} -name "${BASE}*.dar"|wc -l)
+    SLICE_SIZE=$(ls -l --block-size=G "${MOUNT_POINT}/${BASE}.1.dar"|cut -d" " -f5)
+    dar -l  "$MOUNT_POINT/${BASE}" > "$FILELIST" 
+    RESULT=$?
+    if [[ $RESULT == "0" ]]; then
+        SAVED_NO=$(grep -E -c "[Saved].*?] +-"  "$FILELIST")
+        REMOVED_NO=$(grep -c " REMOVED ENTRY " "$FILELIST")        
+    else 
+        SAVED_NO="n/a"
+        REMOVED_NO="n/a"
+    fi
+    rm ${FILELIST}
+    DISK_USAGE_TOTAL=$(du -sch ${MOUNT_POINT}/${BASE}.*|grep -E "total"|cut -f1)
+    printf "%-30s slices: %-3s (%s) total: %-6s Saved: %-7s Removed: %-7s \n" ${BASE} ${NO_SLICES} ${SLICE_SIZE} ${DISK_USAGE_TOTAL} ${SAVED_NO} ${REMOVED_NO}
 done
-DISK_USAGE_TOTAL=`du -sch ${MOUNT_POINT}|egrep "total"|cut -f1`
-printf "Total disk usage in %s: %s \n" ${MOUNT_POINT} ${DISK_USAGE_TOTAL}
+DISK_USAGE_TOTAL=$(du -sch ${MOUNT_POINT}|grep -E "total"|cut -f1)
+printf "Total disk usage in %s %s \n " ${MOUNT_POINT} ${DISK_USAGE_TOTAL}
