a9ad79bbd5aaccf022e33b5072f3e1ce7a9d243f
Per Jensen <per2jensen@gmail.com>
2022-04-15 11:45:07 +0200

    also count symlinks

--- test/test-listfiles-for-FULL.sh
+++ test/test-listfiles-for-FULL.sh
@@ -2,8 +2,8 @@
 
 # Test that the listFiles shows same number as files actually backed up
 
-SCRIPTPATH=`realpath $0`
-SCRIPTDIRPATH=`dirname $SCRIPTPATH`
+SCRIPTPATH=$(realpath $0)
+SCRIPTDIRPATH=$(dirname $SCRIPTPATH)
 echo SCRIPTDIRPATH: $SCRIPTDIRPATH
 
 source $SCRIPTDIRPATH/setup.sh
@@ -17,7 +17,7 @@ NO_BACKEDUP=$(dar -vt -l "$MOUNT_POINT/TEST_FULL_$DATE" |grep "\[Saved\]"|grep -
 # run listFiles
 $TESTDIR/bin/dar-backup.sh -d TEST --list-files --local-backup-dir
 failOnError $?
-NO_LISTED=$(cat /tmp/dar-FULL-filelist.txt|grep -i "adding file"|wc -l)
+NO_LISTED=$(cat /tmp/dar-FULL-filelist.txt|grep -E -i "adding file|adding symlink"|wc -l)
 
 if [[ $NO_BACKEDUP != $NO_LISTED ]]; then
     TESTRESULT=1
