4749356ae7d6844605ace859b61db1d2b849577c
Per Jensen <per2jensen@gmail.com>
2022-04-23 10:39:05 +0200

    make btrfs system to backup from, requires sudo

--- test/test-fsa-none.sh
+++ test/test-fsa-none.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-# test that the --fsa-scope-none option works 
+# this needs sudo to mount a btrfs file system
 
+# test that the --fsa-scope-none option works
+# setup a btrfs file system, backup and restore to another type of filesystem
+
+# setup a btrfs file system
 # run install.sh
 # run dar-backup.sh
 # add file GREENLAND.JEP to include dir and to the exclude dir
@@ -14,6 +18,26 @@ echo SCRIPTDIRPATH: "$SCRIPTDIRPATH"
 
 source "$SCRIPTDIRPATH/setup.sh"
 source "$TESTDIR/conf/dar-backup.conf"
+
+BTRFS_FILE="/tmp/btrfs-file"
+BTRFS_MOUNT_POINT="/tmp/mnt/btrfs"
+
+# setup a btrfs filesystem
+dd if=/dev/zero of="$BTRFS_FILE" bs=1024 count=150000
+mkfs.btrfs "$BTRFS_FILE"
+sudo rm -fr "$BTRFS_MOUNT_POINT"
+mkdir -p "$BTRFS_MOUNT_POINT"
+sudo mount "$BTRFS_FILE" "$BTRFS_MOUNT_POINT"
+sudo chmod 777 "$BTRFS_MOUNT_POINT"
+cp -R "$TESTDIR" "$BTRFS_MOUNT_POINT"
+"$BTRFS_MOUNT_POINT"/dar-backup-test/bin/install.sh
+
+
+
+# set new TESTDIR location
+TESTDIR="$BTRFS_MOUNT_POINT"/dar-backup-test
+MOUNT_POINT="$TESTDIR/archives"
+LOG_LOCATION="$MOUNT_POINT"
 
 # run the test
 "$TESTDIR/bin/dar-backup.sh" -d TEST --local-backup-dir --fsa-scope-none
@@ -50,7 +74,7 @@ fi
 touch "$TESTDIR/dirs/include this one/GREENLAND.JPEG"
 
 # run INCREMENTAL backup
-"$TESTDIR/bin/dar-inc-backup.sh" -d TEST --local-backup-dir --fsa-scope-none
+"$TESTDIR/bin/dar-inc-backup.sh" -d TEST --local-backup-dir  --fsa-scope-none
 RESULT=$?
 if [[ $RESULT != "0" ]]; then
     TESTRESULT=1
@@ -67,6 +91,8 @@ fi
 
 if [[ $TESTRESULT != "0" ]]; then
     echo "Something went wrong, exiting"
+    sudo umount "$BTRFS_MOUNT_POINT"
+    rm -fr "$BTRFS_FILE"
     exit 1
 fi
 
@@ -105,6 +131,10 @@ else
     echo "error more than one file saved in the INCREMENTAL archive"
     TESTRESULT=1
 fi
+
+sudo umount "$BTRFS_MOUNT_POINT"
+rm -fr "$BTRFS_FILE"
+
 
 echo TEST RESULT: "$TESTRESULT"
 exit "$TESTRESULT"
