907deb573f73b06ec297ea719d2fe5c99276183c
Per Jensen <per2jensen@gmail.com>
2022-05-07 14:32:19 +0200

    ....

--- test/test-install-package.sh
+++ test/test-install-package.sh
@@ -7,10 +7,8 @@ _DIR=$(pwd)
 # check if this is running in a Github Action
 if [[ -d "/home/runner/work/dar-backup/dar-backup" ]]; then
     cd "/home/runner/work/dar-backup/dar-backup"
-    pwd
-    git status
-    git tag
-    export TAG=$(git tag |sort -V|tail -n 1)
+    git tag -list "v*"
+    export TAG=$(git tag --list "v*"|sort -V|tail -n 1)
     cd "$_DIR"
 fi
 export RESULT=0
