eace08bd35a03a9d131d2cec76024d93a8c44d76
Per Jensen <per2jensen@gmail.com>
2023-01-31 21:59:28 +0100

    improved catalog handling, expanded test case

--- bin/manager.sh
+++ bin/manager.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 #
-# This script creates the dar catalog from existing dar archives.
+# This script creates/maintains the dar catalogs from existing dar archives for backup definitions.
 #
-# It is used for initialization of the catalog.
-#
+
+# TODO skal LOCAL_BACKUP_DIR bruges ?
+
 LOCAL_BACKUP_DIR=""
 ALTERNATE_ARCHIVE_DIR=""
 CREATE_CATALOG=""
+ADD_DIR=""
 ARCHIVE_DIR_TO_ADD=""
 
 SCRIPTPATH=$(realpath "$0")
@@ -27,11 +29,12 @@ while [ -n "$1" ]; do
           CREATE_CATALOG="1"
           ;;
       --add-dir)
+          ADD_DIR="1"
           shift
           ARCHIVE_DIR_TO_ADD="$1"
           ;;
       --help|-h)
-          echo "$SCRIPTNAME --help|-h  [--local-backup-dir] [--alternate-archive-dir <directory>] [--cleanup-archive]"
+          echo "$SCRIPTNAME --help|-h  [--local-backup-dir] [--alternate-archive-dir <directory>]"
           echo " --local-backup-dir, don't mount a remote directory for cleanup operations"
           echo " --alternate-archive-dir, cleanup in another directory than the one configured, this probably requires --local-backup-dir also"
           echo " --create-catalog"
@@ -40,7 +43,7 @@ while [ -n "$1" ]; do
           exit
           ;;
       *)
-          echo option \"$1\" not recognized, exiting
+          echo option "\"$1\"" not recognized, exiting
           exit
           ;;
   esac
@@ -50,6 +53,13 @@ done
 
 source "${SCRIPTDIRPATH}/../conf/dar-backup.conf"
 source "${SCRIPTDIRPATH}/dar-util.sh"
+
+if [[ $ADD_DIR == "1" && $ARCHIVE_DIR_TO_ADD == "" ]]; then
+    log "ERROR archive dir not given, exiting"
+    exit 1
+fi
+
+
 
 # set MOUNT_POINT to the alternate archive dir
 # this (most probably) requires the --local-backup-dir option to be set also
@@ -69,6 +79,7 @@ log "======================================================="
 log "Alternate directory: \"$ALTERNATE_ARCHIVE_DIR\""
 log "Specific archive: \"$SPECIFIC_ARCHIVE\""
 log "Create catalog: \"$CREATE_CATALOG\""
+log "Add directory:  \"$ADD_DIR\""
 log "Directory to add to catalog: \"$ARCHIVE_DIR_TO_ADD\""
 
 
@@ -78,39 +89,41 @@ mountPrereqs
 
 # create catalog
 if [[ $CREATE_CATALOG == "1"  ]]; then
-    if [[ -e "$MOUNT_POINT/$CATALOG_DB" ]]; then 
-        log "ERROR  \"$MOUNT_POINT/$CATALOG_DB\" already exists, exiting"
-        exit 1
-    fi
-
-    echo ######################################################################################
-    echo must loop over all backup definitions and create a catalog for each definitions
-    echo "exciting until that code change has been made :-)"
-    echo ######################################################################################
-    exit 1
-
-    log "INFO create catalog DB: \"$MOUNT_POINT/$CATALOG_DB\""    
-    dar_manager --create "$MOUNT_POINT"/"$CATALOG_DB"
-    if [[ $? != "0" ]]; then
-        log "ERROR somethin went wrong creating the catalog: \"$MOUNT_POINT/$CATALOG_DB\""
-        exit $?
-    fi
+    for CURRENT_BACKUPDEF in "${SCRIPTDIRPATH}"/../backups.d/*; do
+        CURRENT_BACKUPDEF=$(basename "$CURRENT_BACKUPDEF")
+        CATALOG=${CURRENT_BACKUPDEF}${CATALOG_SUFFIX}
+        if [[ -e "$MOUNT_POINT/$CATALOG" ]]; then 
+            log "WARN  \"$MOUNT_POINT/$CATALOG\" already exists, go to next"
+        else
+            log "INFO create catalog DB: \"$MOUNT_POINT/$CATALOG\""    
+            dar_manager --create "$MOUNT_POINT"/"$CATALOG"
+            if [[ $? != "0" ]]; then
+                log "ERROR somethin went wrong creating the catalog: \"$MOUNT_POINT/$CATALOG\""
+                exit $?
+            fi
+        fi
+    done
 fi
 
 
 # add archives in $ARCHIVE_DIR_TO_ADD to the catalog
 # loop over all archives in the directory
-if [[ $ARCHIVE_DIR_TO_ADD != "" ]]; then
-    SEARCHCRIT="*.dar"
-    for archive in $(find ${MOUNT_POINT} -type f -name "$SEARCHCRIT"|grep -E ".*_FULL_.*|.*_DIFF_.*|.*_INC_.*"|grep -E "^.*?[0-9]{4}-[0-9]{2}-[0-9]{2}" -o|sort -u|sort -s); do
-        BASE=$(basename ${archive})
-        log "INFO add \"$MOUNT_POINT/$BASE\" to catalog"
-        dar_manager --base "$MOUNT_POINT/$CATALOG_DB"  --add $(realpath "$MOUNT_POINT/$BASE")
-        RESULT=$?
-        if [[ $RESULT != "0" ]]; then
-            log "ERROR something went wrong, dar error: \"$RESULT\""
-            exit $RESULT
-        fi
+if [[  $ADD_DIR == "1" && $ARCHIVE_DIR_TO_ADD != "" ]]; then
+    for CURRENT_BACKUPDEF in "${SCRIPTDIRPATH}"/../backups.d/*; do
+        CURRENT_BACKUPDEF=$(basename "$CURRENT_BACKUPDEF")
+        CATALOG=${CURRENT_BACKUPDEF}${CATALOG_SUFFIX}
+        log "Now adding archives for backup definition \"$CURRENT_BACKUPDEF\" to catalog \"$CATALOG\""
+        SEARCHCRIT="${CURRENT_BACKUPDEF}*.dar"
+        for archive in $(find "${MOUNT_POINT}" -type f -name "$SEARCHCRIT"|grep -E "${CURRENT_BACKUPDEF}_FULL_.*|${CURRENT_BACKUPDEF}_DIFF_.*|${CURRENT_BACKUPDEF}_INC_.*"|grep -E "^.*?[0-9]{4}-[0-9]{2}-[0-9]{2}" -o|sort -u|sort -s); do
+            ARCHIVE=$(basename "${archive}")
+            log "INFO add \"$MOUNT_POINT/$ARCHIVE\" to catalog \"$CATALOG\""
+            dar_manager --base "$MOUNT_POINT/$CATALOG"  --add $(realpath "$MOUNT_POINT/$ARCHIVE")
+            RESULT=$?
+            if [[ $RESULT != "0" ]]; then
+                log "ERROR something went wrong, dar error: \"$RESULT\""
+                exit $RESULT
+            fi
+        done
     done
 fi
 log "$SCRIPTNAME ended normally"
--- test/test-create-catalog.sh
+++ test/test-create-catalog.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
 # test creation of catalog
+#   - make 5 backup definitions   
 #   - do a backup
 #   - create the catalog
 #   - populate the catalog with archive data
+#   - list catalog
+#   - check catalog
 #   - restore files
 
 TESTRESULT=0
@@ -15,48 +18,81 @@ echo SCRIPTDIRPATH: "$SCRIPTDIRPATH"
 source "$SCRIPTDIRPATH/setup.sh"
 source "$TESTDIR/conf/dar-backup.conf"
 
-# do a backup
-"$TESTDIR/bin/dar-backup.sh" -d TEST --local-backup-dir
+# generate 5 different backups
+cp "$TESTDIR"/backups.d/TEST "$TESTDIR"/backups.d/TEST2 
+cp "$TESTDIR"/backups.d/TEST "$TESTDIR"/backups.d/TEST3
+cp "$TESTDIR"/backups.d/TEST "$TESTDIR"/backups.d/TEST4 
+cp "$TESTDIR"/backups.d/TEST "$TESTDIR"/backups.d/TEST5 
+
+# do backups
+"$TESTDIR/bin/dar-backup.sh" --local-backup-dir
 RESULT=$?
 if [[ $RESULT != "0" ]]; then
     TESTRESULT=1
 fi
 
-# create catalog
+# create catalogs
 "$TESTDIR/bin/manager.sh" --create-catalog --local-backup-dir
 if [[ $? != "0" ]]; then
   echo ERROR catalog was not created, exiting
   exit 1
 fi
 
-# populate catalog with archive data
-"$TESTDIR/bin/manager.sh"   --add-dir  "$MOUNT_POINT"  --local-backup-dir
+# populate catalogs with archive data
+"$TESTDIR/bin/manager.sh"   --add-dir  "$TESTDIR"/archives  --local-backup-dir
 if [[ $? != "0" ]]; then
   echo ERROR some or all archives were not added to catalog, exiting
   exit 1
 fi
 
 # list the catalog
-dar_manager  -l --base "$TESTDIR"/archives/dar-backup.catalog
-if [[ $? != "0" ]]; then
-  echo ERROR catalog --list failed, exiting
-  exit 1
-fi
+while IFS= read -r -d "" file
+do
+    CURRENT_BACKUPDEF=$(basename "$file")
+    CATALOG=${CURRENT_BACKUPDEF}${CATALOG_SUFFIX}
+    echo "List catalog \"$CATALOG\" for backup definition \"$CURRENT_BACKUPDEF\""
+    dar_manager  -l --base "$(realpath "$TESTDIR"/archives/"$CATALOG")"
+    if [[ $? != "0" ]]; then
+      echo ERROR catalog --list for failed "\"$CATALOG\""
+      TESTRESULT=1
+    fi
+done <  <(find "${TESTDIR}"/backups.d -type f -print0)
 
-# check catalog DB
-dar_manager  -c --base "$TESTDIR"/archives/dar-backup.catalog
-if [[ $? != "0" ]]; then
-  echo ERROR catalog DB is not OK, exiting
-  exit 1
-fi
 
-# restore files to /tmp/dir/
-TEMPDIR=$(mktemp -d)
-echo restoring to \"$TEMPDIR\"
-dar_manager  --base "$TESTDIR"/archives/dar-backup.catalog -e "-R $TEMPDIR" -r "dirs"
-if [[ $? != "0" ]]; then
-  echo ERROR dar_manager restore failed
-  exit 1
-fi
+# check catalogs
+while IFS= read -r -d "" file
+do
+    CURRENT_BACKUPDEF=$(basename "$file")
+    CATALOG=${CURRENT_BACKUPDEF}${CATALOG_SUFFIX}
+    echo check catalog "\"$CATALOG\""
+    if [[ -e "$TESTDIR"/archives/"$CATALOG" ]]; then 
+      dar_manager -c --base "$(realpath "$TESTDIR"/archives/"${CATALOG}")"
+      if [[ $? != "0" ]]; then
+        echo ERROR catalog DB "\"$CATALOG\"" is not OK
+        TESTRESULT=1
+      fi
+    else
+        echo "ERROR  \"${TESTDIR}/archives/${CATALOG}\" does not exist"
+        TESTRESULT=1
+    fi
+done <  <(find "${TESTDIR}"/backups.d -type f -print0)
+
+
+# restore files to temp dirs from catalogs
+while IFS= read -r -d "" file
+do
+    CURRENT_BACKUPDEF=$(basename "$file")
+    CATALOG=${CURRENT_BACKUPDEF}${CATALOG_SUFFIX}
+
+    TEMPDIR=$(mktemp -d)
+    echo restoring "\"$CURRENT_BACKUPDEF\""  to "\"$TEMPDIR\"" from catalog "\"$CATALOG\""
+    dar_manager  --base "$(realpath "$TESTDIR"/archives/"$CATALOG")" -e "-R $TEMPDIR" -r "dirs"
+    if [[ $? != "0" ]]; then
+      echo ERROR dar_manager restore failed
+      TESTRESULT=1
+    fi
+    find "$TEMPDIR" -type f 
+    rm -fr "$TEMPDIR"
+done  <  <(find "${TESTDIR}"/backups.d -type f -print0)
 
 exit "$TESTRESULT"
