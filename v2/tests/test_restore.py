8d36cecd411c6ba46210e3023563c093b23cf385
Per Jensen <per2jensen@gmail.com>
2025-01-04 16:53:30 +0100

    --restore-dir test case more solid

--- v2/tests/test_restore.py
+++ v2/tests/test_restore.py
@@ -1,17 +1,20 @@
 # modified: 2021-07-25 to be a pytest test
-import importlib
 import re
-import sys
-import os
 
 from tests.envdata import EnvData
 from dar_backup.util import run_command
 
 def test_restoredir_requires_value(setup_environment, env):
-    command = ['dar-backup', '--restore', '--restore-dir', , '--log-stdout']
+    command = ['dar-backup', '--restore', '--restore-dir', '--log-stdout', '--log-level', 'debug', '--config-file', env.config_file]
     process = run_command(command)
+    env.logger.info(f"process.returncode={process.returncode}")
     if process.returncode == 0:
         raise Exception(f'dar-backup must fail because a value to --restore-dir is not given')
+    else:
+        stdout, stderr = process.communicate()
+        if not re.search('usage: dar-backup', stderr):
+            raise Exception(f"Expected error message not found in stderr: {stderr}")
+        env.logger.info(f"process.returncode={process.returncode} which is expected")
 
 
                         
