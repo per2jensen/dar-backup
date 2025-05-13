9266a7b8e421ebb83c8342c14eb73b0ad673be55
Per Jensen <per2jensen@gmail.com>
2025-05-13 21:10:36 +0200

    bump to 0.7.1

--- v2/src/dar_backup/__about__.py
+++ v2/src/dar_backup/__about__.py
@@ -1,4 +1,4 @@
-__version__ = "0.6.21"
+__version__ = "0.7.1"
 
 __license__ = '''Licensed under GNU GENERAL PUBLIC LICENSE v3, see the supplied file "LICENSE" for details.
 THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW, not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
