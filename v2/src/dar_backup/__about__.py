7851a35c16100b870f3408a6b7bb3d8c123d1915
Per Jensen <per2jensen@gmail.com>
2025-06-08 06:15:27 +0200

    version 0.8.0 - closing in on 1.0

--- v2/src/dar_backup/__about__.py
+++ v2/src/dar_backup/__about__.py
@@ -1,4 +1,4 @@
-__version__ = "0.7.2"
+__version__ = "0.8.0"
 
 __license__ = '''Licensed under GNU GENERAL PUBLIC LICENSE v3, see the supplied file "LICENSE" for details.
 THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW, not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
